3617158915104c83b9878ae6142a2879f166b581
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-18 19:44:14 +0200

    the header of the project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+  return (
+    <header className="bg-linear-to-r from-[#672280] to-[#A626D3] flex space-x-5 w-full h-12 items-center p-2">
+      <img src='/trollface.png' className="w-[31.31px] h-[26.21px]" />
+      <h1 className="text-2xl font-bold">Meme Generator</h1>
+    </header>
+  );
+}
